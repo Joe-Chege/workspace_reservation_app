e1310dd937ab1de0da27083a1d5037cfa3016814
Joe-Chege <njorogejosephchege@gmail.com>
2024-03-20 14:10:39 +0300

    Update resources/routes.py

---
+++ app/resources/routes.py
@@ -0,0 +1,8 @@
+# app/resources/routes.py
+from flask import Blueprint, render_template
+
+resources_blueprint = Blueprint('resources', __name__)
+
+@resources_blueprint.route('/')
+def index():
+    return render_template('resources/index.html')
