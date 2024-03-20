66b2e0df2551d97e7d21e77a4401a6f8127e72f3
Joe-Chege <njorogejosephchege@gmail.com>
2024-03-20 14:10:38 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
-# app/__init__.py
 from flask import Flask
 from flask_login import LoginManager
 from .extensions import db, migrate
 from .models.user import User
-from .config import Config  # Add this line
+from .config import Config
+from .main.routes import main_bp
+from .bookings.routes import bookings_blueprint
+from .rooms.routes import rooms_blueprint
+from .resources.routes import resources_blueprint
+from .auth.routes import auth_bp
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)  # Config is now defined
+    app.config.from_object(Config)
 
     # Initialize Flask extensions
     db.init_app(app)
@@ -23,4 +27,11 @@ def create_app():
     def load_user(user_id):
         return User.query.get(int(user_id))
     
+    # Register blueprints
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(bookings_blueprint)
+    app.register_blueprint(rooms_blueprint)
+    app.register_blueprint(resources_blueprint)
+
     return app
